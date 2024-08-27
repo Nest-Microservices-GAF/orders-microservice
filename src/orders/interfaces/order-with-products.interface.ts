fcd8a6f66f6f8f2dfe7a4cc220c235391d9ba421
Giovanni Ascarza Hinostroza <gascarzah@gmail.com>
2024-08-26 22:37:49 -0500

    para orders

--- src/orders/interfaces/order-with-products.interface.ts
+++ src/orders/interfaces/order-with-products.interface.ts
@@ -1,18 +1,19 @@
-import { OrderStatus } from "@prisma/client";
+import { OrderStatus } from '@prisma/client';
+
 
 export interface OrderWithProducts {
-    OrderItem: {
-        name: any;
-        productId: number;
-        quantity: number;
-        price: number;
-    }[];
-    id: string;
-    totalAmount: number;
-    totalItems: number;
-    status: OrderStatus;
-    paid: boolean;
-    paidAt: Date | null;
-    createAt: Date;
-    updateAt: Date;
+  OrderItem: {
+      name: any;
+      productId: number;
+      quantity: number;
+      price: number;
+  }[];
+  id: string;
+  totalAmount: number;
+  totalItems: number;
+  status: OrderStatus;
+  paid: boolean;
+  paidAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,7 +2,6 @@ import { Controller, NotImplementedException, ParseUUIDPipe } from '@nestjs/comm
 import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto, PaidOrderDto } from './dto';
 
@@ -12,49 +11,36 @@ export class OrdersController {
 
   @MessagePattern('createOrder')
   async create(@Payload() createOrderDto: CreateOrderDto) {
-    console.log('llega OrdersController createOrder')
+
     const order = await this.ordersService.create(createOrderDto);
     const paymentSession = await this.ordersService.createPaymentSession(order)
+
     return {
-      order, 
-      paymentSession
+      order,
+      paymentSession,
     }
   }
-  // @MessagePattern('createOrder')
-  // create(@Payload() createOrderDto: CreateOrderDto) {
-  //   return this.ordersService.create(createOrderDto);
-  // }
 
   @MessagePattern('findAllOrders')
-  findAll(@Payload()orderPaginationDto: OrderPaginationDto) {
+  findAll(@Payload() orderPaginationDto: OrderPaginationDto ) {
     return this.ordersService.findAll(orderPaginationDto);
   }
 
   @MessagePattern('findOneOrder')
-  findOne(@Payload('id',ParseUUIDPipe) id: string) {
+  findOne(@Payload('id', ParseUUIDPipe ) id: string) {
     return this.ordersService.findOne(id);
   }
 
-  @MessagePattern('updateOrder')
-  update(@Payload() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(updateOrderDto.id, updateOrderDto);
-  }
-
-  @MessagePattern('removeOrder')
-  remove(@Payload() id: number) {
-    return this.ordersService.remove(id);
-  }
-
   @MessagePattern('changeOrderStatus')
-  changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto){
+  changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto ) {
     return this.ordersService.changeStatus(changeOrderStatusDto)
-  }
-
-  // @MessagePattern()
-  @EventPattern('payment.suceeded')
-  paidOrder(@Payload() paidOrderDto: PaidOrderDto){
-    console.log({paidOrderDto})
-    return this.ordersService.paidOrder(paidOrderDto)
+    
   }
   
+  
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto: PaidOrderDto ) {
+    return this.ordersService.paidOrder( paidOrderDto );
+  }
+
 }
